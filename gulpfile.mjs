5bbb1c36b4dd632e9234072c8445c783b3f44359
JuliaYarlykova <yarlykova2506@gmail.com>
2023-11-20 17:42:48 +0700

    mistake GET

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -45,7 +45,7 @@ function clean(){
 
 function includeHtml(){
     return gulp
-        .src("src/html/*/html")
+        .src("src/html/*.html")
         .pipe(plumber())
         .pipe(
             include({
